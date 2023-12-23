b45fbf630fb8db2163a31515701f870eac88bd10
Boaz Cohen <boazprog@gmail.com>
2023-12-23 11:26:04 +0200

    templ

--- internal/routes/base.go
+++ internal/routes/base.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"bez/bez_server/templates"
+
+	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,8 +13,12 @@ func Init() {
 	router.GET("/v", func(c *gin.Context) {
 		c.String(200, "v0.0.2")
 	})
-	// component := templates.Hello("world")
-	// router.GET("/hello", templ.Handler(component))
+	component := templates.Hello("Shirit")
+	router.GET("/hello", gin.WrapH(templ.Handler(component)))
+
+	usersComponent := templates.Users([]string{"one", "two", "three"})
+	router.GET("/users", gin.WrapH(templ.Handler(usersComponent)))
+
 	usersInit()
 	router.Run(":8080")
 }
